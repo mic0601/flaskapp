aa07d70fab6fcf0121c4897a828abd1b3bf91adc
SakuraGaara <mic0601@163.com>
2019-11-06 16:57:42 +0800

    Update index.py
    Version 1.

--- app/web/index.py
+++ app/web/index.py
@@ -6,7 +6,7 @@ from app.helper.local import getLocal
 @web.route('/index/')
 def index():
     Local=getLocal()
-    return 'Flask v1: %s | %s | %s' % (Local.getcurrenttime(),
+    return 'Flask v1 | %s | %s | %s' % (Local.getcurrenttime(),
                              Local.gethostname(),
                              Local.getlocaladdress())
 
